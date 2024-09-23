f5febd70517c29bd7ff6c4fe4d3f2352b8e2f940
kaviyagg <96510867+kaviyagg@users.noreply.github.com>
2024-09-23 10:19:56 +0530

    login page commit

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -2,32 +2,30 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'; // Add this line
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule], // Add FormsModule and ReactiveFormsModule here
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './loginpage.component.html',
   styleUrls: ['./loginpage.component.scss']
 })
 export class LoginpageComponent {
  emailValidator = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-  loginForm: FormGroup;
   http = inject(HttpClient);
+  private formBuilder = inject(FormBuilder);
 
 
-  constructor(private formBuilder: FormBuilder) {
-    this.loginForm = this.createLoginForm();
+  constructor() {
+
   }
 
-  createLoginForm() {
-    return this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email,Validators.pattern(this.emailValidator)]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
-  }
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(this.emailValidator)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
 
   onSubmit() {
     this.http.post("https://api.weightgurus.com/v3/account/login", this.loginForm.value).subscribe((res: any) => {
